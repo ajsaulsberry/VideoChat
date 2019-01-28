a8da965e7285ddd99f169b41ce9bf14ea0b6892d
A. J. Saulsberry <ajsaulsberry@gmail.com>
2019-01-28 16:57:52 -0500

    Added empty components and services.

--- VideoChat/ClientApp/src/app/app.module.ts
+++ VideoChat/ClientApp/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
+import { HomeComponent } from './home/home.component';
+import { ParticipantsComponent } from './participants/participants.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SettingsComponent } from './settings/settings.component';
+import { DeviceSelectComponent } from './settings/device-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent,
+    HomeComponent,
+    ParticipantsComponent,
+    RoomsComponent,
+    SettingsComponent,
+    DeviceSelectComponent
   ],
   imports: [
     BrowserModule
---
+++ VideoChat/ClientApp/src/app/settings/device-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-select',
+  templateUrl: './device-select.component.html',
+  styleUrls: ['./device-select.component.css']
+})
+export class DeviceSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
